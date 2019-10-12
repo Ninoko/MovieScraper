645a6e01c63682e0e6ea7f2d227bbaa99fb941d4
Niniko <miloszmedia96@gmail.com>
2019-10-12 03:22:54 +0200

    setup created

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scraping',
+    version='1.0.0',
+    author='Miłosz Michta',
+    author_email='milosz.casper.michta@gmail.com',
+    description='Python package for movie data scraping',
+    packages=find_packages(include=['scraping']),
+    install_requires=(
+        'bs4',
+        'requests',
+    ),
+)
