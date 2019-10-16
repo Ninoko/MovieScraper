e0b8b33e55cdac14b310dd9552c44250c59cebe5
Niniko <miloszmedia96@gmail.com>
2019-10-16 20:18:29 +0200

    person scraper reconstructed

--- scraping/person_scraper.py
+++ scraping/person_scraper.py
@@ -9,53 +9,63 @@ from scraping.utils import safe_return, set_locale
 
 class PersonScraper(BaseScraper):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, person_url: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.person_soup = None
+        self.person_url = person_url
+        self._person_soup = None
+        self._professions = None
+
+    @property
+    def person_soup(self) -> BeautifulSoup:
+        if self._person_soup is None:
+            self._person_soup = self.get_soup(self.person_url)
+        return self._person_soup\
+
+    @property
+    def professions(self) -> List[str]:
+        if self._professions is None:
+            _professions = self.person_soup.find(
+                'div', {'class': 'communityRate'}
+            ).find_all('div', id=lambda id: id and id.startswith('pr_'), recursive=False)
+            self._professions = [profession['data-prof'] for profession in _professions]
+        return self._professions
 
     @safe_return
-    def get_full_name(self, person_soup: BeautifulSoup) -> str:
-        return person_soup.find('h1', {'class', 'personName'}).text
+    def get_full_name(self) -> str:
+        return self.person_soup.find('h1', {'class', 'personName'}).text
 
     @safe_return
-    def get_image_url(self, person_soup: BeautifulSoup) -> str:
-        return person_soup.find('img', {'itemprop': 'image'})['src']
+    def get_image_url(self) -> str:
+        return self.person_soup.find('img', {'itemprop': 'image'})['src']
 
-    @safe_return
-    def get_birth_date(self, person_soup: BeautifulSoup) -> datetime.date:
+    # @safe_return
+    def get_birth_date(self) -> datetime.date:
         return datetime.strptime(
-            person_soup.find(
+            self.person_soup.find(
                 'span', {'itemprop': 'birthDate'}
             ).text.strip(), '%d %B %Y').date()
 
     @safe_return
-    def get_death_date(self, person_soup: BeautifulSoup) -> datetime.date:
+    def get_death_date(self) -> datetime.date:
         return datetime.strptime(
-            person_soup.find(
+            self.person_soup.find(
                 'span', {'itemprop': 'deathDate'}
             ).text.strip(), '%d %B %Y').date()
 
-    def person_description(self, person_url: str) -> Dict[str, Any]:
-        self.person_soup = self.get_soup(person_url)
+    def get_person_description(self) -> Dict[str, Any]:
         return {
-            'name': self.get_full_name(self.person_soup),
-            'birth_date': self.get_birth_date(self.person_soup),
-            'death_date': self.get_death_date(self.person_soup),
-            'image_url': self.get_image_url(self.person_soup),
+            'name': self.get_full_name(),
+            'birth_date': self.get_birth_date(),
+            'death_date': self.get_death_date(),
+            'image_url': self.get_image_url(),
         }
 
-
-class ActorScraper(PersonScraper):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     @safe_return
-    def get_rating(self, actor_soup: BeautifulSoup) -> float:
+    def get_rating(self, profession: str) -> float:
         return float(
-            actor_soup.find('div', {'data-prof': 'actors'})
-                      .find('span', {'itemprop': 'ratingValue'})
-                      .text.replace(',', '.'))
+            self.person_soup.find('div', {'data-prof': profession})
+                            .find('span', {'itemprop': 'ratingValue'})
+                            .text.replace(',', '.'))
 
     @safe_return(exception=AttributeError)
     def _get_role_name(self, role_soup: BeautifulSoup) -> str:
@@ -68,25 +78,34 @@ class ActorScraper(PersonScraper):
             'movie_url': f'{self.website}{movie["href"].strip()}',
             'role_name': self._get_role_name(role_soup)}
 
-    def get_roles(self, actor_soup) -> List[Dict[str, Any]]:
-        rows = actor_soup.find('tbody', attrs={'data-profession': 'actors'}).find_all('tr')
+    def get_roles(self, profession: str) -> List[Dict[str, Any]]:
+        rows = self.person_soup.find(
+            'tbody', attrs={'data-profession': profession}
+        ).find_all('tr')
         return [self.get_role(row) for row in rows]
 
-    def actor_description(self, actor_url: str) -> Dict[str, Any]:
-        person_description = super().person_description(actor_url)
+    def get_profession_description(self, profession: str) -> Dict[str, Any]:
         return {
-            'rating': self.get_rating(self.person_soup),
-            'roles': self.get_roles(self.person_soup),
-            **person_description
+            'rating': self.get_rating(profession),
+            'roles': self.get_roles(profession),
+        }
+
+    def get_description(self):
+        return {
+            **self.get_person_description(),
+            'professions': {
+                profession: self.get_profession_description(profession)
+                for profession in self.professions
+            }
         }
 
 
 if __name__ == '__main__':
     set_locale()
-    actor_scraper = ActorScraper()
-    desc = actor_scraper.actor_description("https://www.filmweb.pl/person/Johnny.Depp")
-    for role in desc['roles']:
-        print(f'{desc["name"]} play in the {role["movie_title"]} as {role["role_name"]}')
+    actor_scraper = PersonScraper("https://www.filmweb.pl/person/Johnny.Depp")
+    desc = actor_scraper.get_description()
     print(desc.keys())
-    print(desc['rating'])
     print(desc['birth_date'])
+    for prof in desc['professions']:
+        print(prof)
+        print(desc['professions'][prof])
