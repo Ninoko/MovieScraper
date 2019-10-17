c39e16a20fc9652f45ca8646b5f62e62833020e9
Niniko <miloszmedia96@gmail.com>
2019-10-17 02:09:24 +0200

    naming specs

--- scraping/person_scraper.py
+++ scraping/person_scraper.py
@@ -19,7 +19,7 @@ class PersonScraper(BaseScraper):
     def person_soup(self) -> BeautifulSoup:
         if self._person_soup is None:
             self._person_soup = self.get_soup(self.person_url)
-        return self._person_soup\
+        return self._person_soup
 
     @property
     def professions(self) -> List[str]:
@@ -31,70 +31,70 @@ class PersonScraper(BaseScraper):
         return self._professions
 
     @safe_return
-    def get_full_name(self) -> str:
+    def full_name(self) -> str:
         return self.person_soup.find('h1', {'class', 'personName'}).text
 
     @safe_return
-    def get_image_url(self) -> str:
+    def image_url(self) -> str:
         return self.person_soup.find('img', {'itemprop': 'image'})['src']
 
     @safe_return
-    def get_birth_date(self) -> datetime.date:
+    def birth_date(self) -> datetime.date:
         return datetime.strptime(
             self.person_soup.find(
                 'span', {'itemprop': 'birthDate'}
             ).text.strip(), '%d %B %Y').date()
 
     @safe_return
-    def get_death_date(self) -> datetime.date:
+    def death_date(self) -> datetime.date:
         return datetime.strptime(
             self.person_soup.find(
                 'span', {'itemprop': 'deathDate'}
             ).text.strip(), '%d %B %Y').date()
 
-    def get_person_description(self) -> Dict[str, Any]:
+    def person_description(self) -> Dict[str, Any]:
         return {
-            'name': self.get_full_name(),
-            'birth_date': self.get_birth_date(),
-            'death_date': self.get_death_date(),
-            'image_url': self.get_image_url(),
+            'full_name': self.full_name(),
+            'birth_date': self.birth_date(),
+            'death_date': self.death_date(),
+            'image_url': self.image_url(),
         }
 
     @safe_return
-    def get_rating(self, profession: str) -> float:
+    def rating(self, profession: str) -> float:
         return float(
             self.person_soup.find('div', {'data-prof': profession})
                             .find('span', {'itemprop': 'ratingValue'})
                             .text.replace(',', '.'))
 
     @safe_return(exception=AttributeError)
-    def _get_role_name(self, role_soup: BeautifulSoup) -> str:
+    def _role_name(self, role_soup: BeautifulSoup) -> str:
         return role_soup.find('td', {'class', 'rt'}).find('p', {'class', 'roleText'}).text
 
-    def get_role(self, role_soup: BeautifulSoup) -> Dict[str, Any]:
+    def _role(self, role_soup: BeautifulSoup) -> Dict[str, Any]:
         movie = role_soup.find('td', {'class', 'ft'}).find('a')
         return {
             'movie_title': movie.text,
             'movie_url': f'{self.website}{movie["href"].strip()}',
-            'role_name': self._get_role_name(role_soup)}
+            'role_name': self._role_name(role_soup)}
 
-    def get_roles(self, profession: str) -> List[Dict[str, Any]]:
+    def roles(self, profession: str) -> List[Dict[str, Any]]:
         rows = self.person_soup.find(
-            'tbody', attrs={'data-profession': profession}
+            'tbody', {'data-profession': profession}
         ).find_all('tr')
-        return [self.get_role(row) for row in rows]
+        return [self._role(row) for row in rows]
 
-    def get_profession_description(self, profession: str) -> Dict[str, Any]:
+    def profession_description(self, profession: str) -> Dict[str, Any]:
         return {
-            'rating': self.get_rating(profession),
-            'roles': self.get_roles(profession),
+            'rating': self.rating(profession),
+            'roles': self.roles(profession),
         }
 
     def get_description(self):
         return {
-            **self.get_person_description(),
+            **self.person_description(),
             'professions': {
-                profession: self.get_profession_description(profession)
+                profession: self.profession_description(profession)
                 for profession in self.professions
             }
         }
@@ -102,8 +102,8 @@ class PersonScraper(BaseScraper):
 
 if __name__ == '__main__':
     set_locale()
-    actor_scraper = PersonScraper("https://www.filmweb.pl/person/Johnny.Depp")
-    desc = actor_scraper.get_description()
+    scraper = PersonScraper("https://www.filmweb.pl/person/Johnny.Depp")
+    desc = scraper.get_description()
     print(desc.keys())
     print(desc['birth_date'])
     for prof in desc['professions']:
