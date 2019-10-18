efdff2ad8bfbbdc289578549ab25aa3632f07394
Ninoko <miloszmedia96@gmail.com>
2019-10-18 22:30:32 +0200

    more naming updates

--- scraping/person_scraper.py
+++ scraping/person_scraper.py
@@ -26,7 +26,7 @@ class PersonScraper(BaseScraper):
         if self._professions is None:
             _professions = self.person_soup.find(
                 'div', {'class': 'communityRate'}
-            ).find_all('div', id=lambda id: id and id.startswith('pr_'), recursive=False)
+            ).find_all('div', id=lambda _id: _id and _id.startswith('pr_'), recursive=False)
             self._professions = [profession['data-prof'] for profession in _professions]
         return self._professions
 
@@ -61,7 +61,7 @@ class PersonScraper(BaseScraper):
         }
 
     @safe_return
-    def rating(self, profession: str) -> float:
+    def profession_rating(self, profession: str) -> float:
         return float(
             self.person_soup.find('div', {'data-prof': profession})
                             .find('span', {'itemprop': 'ratingValue'})
@@ -76,9 +76,10 @@ class PersonScraper(BaseScraper):
         return {
             'movie_title': movie.text,
             'movie_url': f'{self.website}{movie["href"].strip()}',
-            'role_name': self._role_name(role_soup)}
+            'role_name': self._role_name(role_soup)
+        }
 
-    def roles(self, profession: str) -> List[Dict[str, Any]]:
+    def profession_roles(self, profession: str) -> List[Dict[str, Any]]:
         rows = self.person_soup.find(
             'tbody', {'data-profession': profession}
         ).find_all('tr')
@@ -86,26 +87,12 @@ class PersonScraper(BaseScraper):
 
     def profession_description(self, profession: str) -> Dict[str, Any]:
         return {
-            'rating': self.rating(profession),
-            'roles': self.roles(profession),
-        }
-
-    def get_description(self):
-        return {
-            **self.person_description(),
-            'professions': {
-                profession: self.profession_description(profession)
-                for profession in self.professions
-            }
+            'rating': self.profession_rating(profession),
+            'roles': self.profession_roles(profession),
         }
 
 
 if __name__ == '__main__':
     set_locale()
     scraper = PersonScraper("https://www.filmweb.pl/person/Johnny.Depp")
-    desc = scraper.get_description()
-    print(desc.keys())
-    print(desc['birth_date'])
-    for prof in desc['professions']:
-        print(prof)
-        print(desc['professions'][prof])
+    print(scraper.profession_description('actors'))
